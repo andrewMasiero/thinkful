b18e92215f1b3f27141915ecbcd3d0bd6c84afef
Andrew Masiero <andymasiero@gmail.com>
2024-07-14 09:30:25 -0500

    complete module 3.3.6

--- node-express-getting-started/src/app.js
+++ node-express-getting-started/src/app.js
@@ -1,4 +1,18 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
+
+app.use(morgan("dev"));
+
+app.get("/state/:abbreva", (req, res, next) => {
+  const abbreviation = req.params.abbreviation;
+
+  if (abbreviation.length !== 2) {
+    next("State abbreviation is invalid.");
+  } else {
+    res.send(`${abbreviation} is a nice state. I'd like to visit.`);
+  }
+});
 
 module.exports = app;
