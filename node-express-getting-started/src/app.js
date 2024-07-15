c56df351c6c5b6b4b30580ed8259a5cf7821f2d6
Andrew Masiero <andymasiero@gmail.com>
2024-07-14 19:18:47 -0500

    starting module 3.3.7 router-level middleware

--- node-express-getting-started/src/app.js
+++ node-express-getting-started/src/app.js
@@ -1,18 +1,22 @@
 const express = require("express");
-const morgan = require("morgan");
-
 const app = express();
 
-app.use(morgan("dev"));
-
-app.get("/state/:abbreva", (req, res, next) => {
+app.get("/states/:abbreviation", (req, res, next) => {
   const abbreviation = req.params.abbreviation;
-
   if (abbreviation.length !== 2) {
     next("State abbreviation is invalid.");
   } else {
-    res.send(`${abbreviation} is a nice state. I'd like to visit.`);
+    res.send(`${abbreviation} is a nice state, I'd like to visit.`);
   }
+});
+
+app.use((req, res, next) => {
+  res.send(`The route ${req.path} does not exist!`);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.send(err);
 });
 
 module.exports = app;
