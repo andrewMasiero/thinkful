00b6c51a787398a78ae09ada23ed7102c3bdbcc0
Andrew Masiero <andymasiero@gmail.com>
2024-07-16 16:10:02 -0500

    reset to state from end of module 3.4.2

--- robust-server-structure/src/app.js
+++ robust-server-structure/src/app.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const app = express();
+const pastes = require("./data/pastes-data");
 
+console.log("Starting module 3.4.2 Static data");
 // TODO: Follow instructions in the checkpoint to implement ths API.
 
-// Not found handler
-app.use((request, response, next) => {
-  next(`Not found: ${request.originalUrl}`);
+app.use("/pastes/:pasteId", (req, res, next) => {
+  const { pasteId } = req.params;
+  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
+
+  if (foundPaste) {
+    res.json({ data: foundPaste });
+  } else {
+    next(`Pastes not found: ${pasteId}`);
+  }
 });
 
-// Error handler
-app.use((error, request, response, next) => {
-  console.error(error);
-  response.send(error);
+app.use("/pastes", (req, res) => {
+  res.json({ data: pastes });
+});
+
+// Not found handler
+app.use((req, res, next) => {
+  next(`Not found: ${req.originalUrl}`);
+});
+
+// err handler
+app.use((err, req, res, next) => {
+  console.err(err);
+  res.send(err);
 });
 
 module.exports = app;
