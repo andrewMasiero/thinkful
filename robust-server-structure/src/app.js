8d1f0314f3f4fb543f75edcbd0e7fc750fcc4b93
Andrew Masiero <andymasiero@gmail.com>
2024-07-16 17:31:20 -0500

    complete module 3.4.3 RESTful APIs

--- robust-server-structure/src/app.js
+++ robust-server-structure/src/app.js
@@ -5,7 +5,8 @@ const pastes = require("./data/pastes-data");
 console.log("Starting module 3.4.2 Static data");
 // TODO: Follow instructions in the checkpoint to implement ths API.
 
-app.use("/pastes/:pasteId", (req, res, next) => {
+app.use(express.json());
+app.get("/pastes/:pasteId", (req, res, next) => {
   const { pasteId } = req.params;
   const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
 
@@ -16,8 +17,30 @@ app.use("/pastes/:pasteId", (req, res, next) => {
   }
 });
 
-app.use("/pastes", (req, res) => {
+app.get("/pastes", (req, res) => {
   res.json({ data: pastes });
+});
+
+let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
+
+app.post("/pastes", (req, res, next) => {
+  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } =
+    req.body;
+  if (text) {
+    const newPaste = {
+      id: ++lastPasteId, // Increment last ID, then assign as the current ID
+      name,
+      syntax,
+      exposure,
+      expiration,
+      text,
+      user_id,
+    };
+    pastes.push(newPaste);
+    res.status(201).json({ data: newPaste });
+  } else {
+    res.sendStatus(400);
+  }
 });
 
 // Not found handler
