643e6b99a07f4269b1879282c7fb34b735776129
Andrew Masiero <andymasiero@gmail.com>
2024-07-16 15:38:34 -0500

    starting module 3.4.3 RESTful APIs

--- robust-server-structure/src/app.js
+++ robust-server-structure/src/app.js
@@ -1,24 +1,7 @@
 const express = require("express");
 const app = express();
-const pastes = require("./data/pastes-data");
 
-console.log("Starting module 3.4.2 Static data");
 // TODO: Follow instructions in the checkpoint to implement ths API.
-
-app.use("/pastes/:pasteId", (req, res, next) => {
-  const { pasteId } = req.params;
-  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
-
-  if (foundPaste) {
-    res.json({ data: foundPaste });
-  } else {
-    next(`Pastes not found: ${pasteId}`);
-  }
-});
-
-app.use("/pastes", (req, res) => {
-  res.json({ data: pastes });
-});
 
 // Not found handler
 app.use((request, response, next) => {
