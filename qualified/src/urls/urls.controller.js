49495fbb7859fe7c3611a75c9f6283a948a07e77
Andrew Masiero <andymasiero@gmail.com>
2024-08-09 20:45:09 -0500

    assessment completed: 3.4.7

--- qualified/src/urls/urls.controller.js
+++ qualified/src/urls/urls.controller.js
@@ -1,4 +1,5 @@
 const urls = require("../data/urls-data");
+const recordUrlUse = require("../uses/uses.controller").recordUrlUse;
 
 let lastUrlId = urls.reduce((maxId, url) => Math.max(maxId, url.id), 0);
 
@@ -10,9 +11,24 @@ function read(req, res, next) {
   res.json({ data: res.locals.url });
 }
 
-function create(req, res) {}
+function create(req, res) {
+  const { data: { href } = {} } = req.body;
+  const newUrl = {
+    href,
+    id: ++lastUrlId,
+  };
+  urls.push(newUrl);
+  res.status(201).json({ data: newUrl });
+}
 
-function update(req, res, next) {}
+function update(req, res, next) {
+  const url = res.locals.url;
+  const { data: { href } = {} } = req.body;
+
+  url.href = href;
+
+  res.json({ data: url });
+}
 
 function urlExists(req, res, next) {
   const urlId = parseInt(req.params.urlId);
@@ -20,8 +36,25 @@ function urlExists(req, res, next) {
   if (res.locals.url) {
     next();
   } else {
-    next({ status: 404, message: "URL not found" });
+    next({ status: 404, message: `URL not found for url id: ${urlId}` });
+    //  an error key set to a message containing the id
   }
 }
 
-module.exports = { list, read: [urlExists, read], urlExists };
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({ status: 400, message: `Must include a ${propertyName}` });
+  };
+}
+
+module.exports = {
+  list,
+  read: [urlExists, recordUrlUse, read],
+  create: [bodyDataHas("href"), create],
+  update: [urlExists, bodyDataHas("href"), update],
+  urlExists,
+};
--- qualified/src/urls/urls.router.js
+++ qualified/src/urls/urls.router.js
@@ -1,8 +1,21 @@
 const router = require("express").Router();
 const controller = require("./urls.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
+const usesRouter = require("../uses/uses.router");
 
-router.route("/").get(controller.list);
+router.use("/:urlId/uses", controller.urlExists, usesRouter);
+router.use("/:urlId/uses/:useId", controller.urlExists, usesRouter);
 
-router.route("/:urlId").get(controller.read);
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
+
+router
+  .route("/:urlId")
+  .get(controller.read)
+  .put(controller.update)
+  .all(methodNotAllowed);
 
 module.exports = router;
